6fab575c8140a9846743678e2a1fed6d82a545cb
Jose Arreluce <arreluce.jose@gmail.com>
2017-05-19 18:21:33 -0500

    Moved ModalBox component to own file

---
+++ src/ModalBox.js
@@ -0,0 +1,54 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactBootstrap = require('react-bootstrap');
+
+var Modal = ReactBootstrap.Modal;
+class ModalBox extends React.Component {
+  constructor () {
+    super();
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.state = {
+      showModal: false
+    };
+  }
+  
+  closeModal () {
+    this.setState({
+      showModal: false
+    });
+  }
+  
+  openModal () {
+    this.setState({
+      showModal: true
+    });
+  }
+  
+  render () {
+    return (
+      <div>
+        <button type="button" onClick={this.openModal}>
+          Open Modal
+        </button>
+        <Modal show={this.state.showModal} >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal Heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4> hello world </h4>
+            <p> This is a modal box.</p>
+            <p> Enjoy ! </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <button onClick={this.closeModal}>
+              Close
+            </button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+module.exports = {ModalBox};
--- src/main.js
+++ src/main.js
@@ -1,56 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactBootstrap = require('react-bootstrap');
-
-var Modal = ReactBootstrap.Modal;
-class ModalBox extends React.Component {
-  constructor () {
-    super();
-    this.closeModal = this.closeModal.bind(this);
-    this.openModal = this.openModal.bind(this);
-    this.state = {
-      showModal: false
-    };
-  }
-  
-  closeModal () {
-    this.setState({
-      showModal: false
-    });
-  }
-  
-  openModal () {
-    this.setState({
-      showModal: true
-    });
-  }
-  
-  render () {
-    return (
-      <div>
-        <button type="button" onClick={this.openModal}>
-          Open Modal
-        </button>
-        <Modal show={this.state.showModal} >
-          <Modal.Header closeButton>
-            <Modal.Title>Modal Heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4> hello world </h4>
-            <p> This is a modal box.</p>
-            <p> Enjoy ! </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <button onClick={this.closeModal}>
-              Close
-            </button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
-}
-
+var Modal = require('./ModalBox.js');
+var ModalBox = Modal.ModalBox;
 class App extends React.Component {
   render () {
     return (
