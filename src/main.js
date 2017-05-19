7dc2bdcb9f0209cbce4b6201b0efee2b7e31a924
Jose Arreluce <arreluce.jose@gmail.com>
2017-05-19 18:13:26 -0500

    Removed unneccessary codepen leftover files, added server.js, placed react code into src directory, index.html in www directory

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactBootstrap = require('react-bootstrap');
+
 var Modal = ReactBootstrap.Modal;
 class ModalBox extends React.Component {
   constructor () {
